b4514b993700fcb3c45de427a62afa21ef86ae6a
Despina22 <stankovskadespina22@gmail.com>
2023-09-06 16:44:57 +0200

    Forkify app

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e5ea8f1e-9fa9-4fc9-9f5d-a45b41050ff4';
+export const MODAL_CLOSE_SECONDS = 2.5;
